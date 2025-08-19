1b9f9b4febba472c416a44b1e0ae05e66fa1a798
Michał <michal@mailx.com.pl>
2025-08-19 15:45:24 +0200

    Update PowerDNS bootstrap initial configurarion, hide API key and zone creation and records using loop instead using values in the scripts

--- PowerDNS/create_zone.sh
+++ PowerDNS/create_zone.sh
@@ -1,49 +1,80 @@
 #!/bin/sh
 
-# Czekanie na uruchomienie usług
+# Waiting for PowerDNS container start
 echo "Czekam na uruchomienie PowerDNS Authoritative i PowerDNS Admin..."
 sleep 20
 
-# Dane do utworzenia strefy
+# variable for zone creation
 ZONE_NAME="in.mailx.com.pl"
 PDNS_API_URL="http://pdns-authoritative:8081"
-PDNS_API_KEY="QqyTpVc9y*8vFcaLUSUDZGTr"
+PDNS_API_KEY=`cat .env | grep SECRET_KEY | awk -f "=" '{print $2}' | tr -d "'"`
+
+declare -A RECORDS
+RECORDS+=(
+  ["dns1"]="172.16.8.250"
+  ["dns2"]="172.16.8.251"
+  ["powerdns"]="172.16.8.254"
+)
 
 echo "Tworzenie strefy $ZONE_NAME..."
 
-curl -X POST \
-  -H "X-API-Key: $PDNS_API_KEY" \
-  -H "Content-Type: application/json" \
-  --data-binary "{
-    \"name\": \"$ZONE_NAME.\",
-    \"kind\": \"Native\",
-    \"masters\": [],
-    \"nameservers\": [\"dns1.$ZONE_NAME.\"]
-  }" \
-  $PDNS_API_URL/api/v1/servers/localhost/zones
+for key in ${RECORDS[@]}; do
+  if [ $key == "dns1" ] || [ $key == "dns2" ]; then
+    curl -X POST \
+      -H "X-API-Key: $PDNS_API_KEY" \
+      -H "Content-Type: application/json" \
+      --data-binary "{
+        \"name\": \"$ZONE_NAME.\",
+        \"kind\": \"Native\",
+        \"masters\": [],
+        \"nameservers\": [\"$key.$ZONE_NAME\"]
+      }" \
+      $PDNS_API_URL/api/v1/servers/localhost/zones
+  fi
+done
 
 echo "Strefa została utworzona. Dodawanie rekordów..."
 
-# Dane do utworzenia rekordu A
-RECORD_NAME="dns1"
-RECORD_TYPE="A"
-RECORD_CONTENT="172.16.8.250"
-
-curl -X PATCH \
-  -H "X-API-Key: $PDNS_API_KEY" \
-  -H "Content-Type: application/json" \
-  --data-binary "{
-    \"rrsets\": [
+# Update A records
+for key in ${!RECORDS[@]}; do
+  curl -X PATCH \
+    -H "X-API-Key: $PDNS_API_KEY" \
+    -H "Content-Type: application/json" \
+    --data-binary "{
+      \"rrsets\": [
       {
-        \"name\": \"$RECORD_NAME.$ZONE_NAME.\",
-        \"type\": \"$RECORD_TYPE\",
+        # for A records
+        \"name\": \"$ZONE_NAME.\",
+        \"type\": \"A\",
         \"ttl\": 3600,
         \"records\": [
-          {
-            \"content\": \"$RECORD_CONTENT\"
-          }
+          { \"content\": \"$key.$ZONE_NAME.\" }
         ]
-      }
-    ]
-  }" \
-  $PDNS_API_URL/api/v1/servers/localhost/zones/$ZONE_NAME.
+      },
+      ]
+    }"
+    $PDNS_API_URL/api/v1/servers/localhost/zones/$ZONE_NAME.
+done
+
+# Update of NS records
+for key in ${!RECORDS[@]}; do
+  if [ $key == "dns1" ] || [ $key == "dns2" ]; then
+    curl -X PATCH \
+      -H "X-API-Key: $PDNS_API_KEY" \
+      -H "Content-Type: application/json" \
+      --data-binary "{
+        \"rrsets\": [
+        {
+          # for NS records
+          \"name\": \"$ZONE_NAME.\",
+          \"type\": \"A\",
+          \"ttl\": 3600,
+          \"records\": [
+            { \"content\": \"$key.$ZONE_NAME.\" }
+          ]
+        },
+        ]
+      }"
+      $PDNS_API_URL/api/v1/servers/localhost/zones/$ZONE_NAME.
+  fi
+done
